8270b25af92e64e73d65b14a8e1429defeaaa886
Ivan Miloslavov <ivan@cyberiam.ca>
2025-03-01 08:37:39 -0500

    Cleanup removed NPM packages

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,1 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// Place any code that should run once, when the application is started
